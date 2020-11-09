7bc40adaed0fcc68506356d2402b2082c63f44d6
Minecraftschurli <minecraftschurli@gmail.com>
2020-11-09 21:32:22 +0100

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 # This call to setup() does all the work
 setup(
     name="flask_tinify",
-    version="1.0",
+    version="1.1",
     description="An adaption of tinify as a flask extension",
     long_description=README,
     long_description_content_type="text/markdown",
